4cdd6edc325602ac81ae9e44270b6691e6f711ef
Tolga Karasay <tolgakarasay@gmail.com>
2022-01-18 00:02:07 +0300

    add routes and config

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
 // MODULES
 const express = require('express');
-const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const ejs = require('ejs');
 const MongoDBStore = require('connect-mongodb-session')(session);
-const userController = require('./controllers/userController');
-const pageController = require('./controllers/pageController');
-const auth = require('./middlewares/authMiddleware');
+const pageRoute = require('./routes/pageRoute');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
 require('dotenv').config();
-
-// DB CONNECTION
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('Database connection successful');
-  })
-  .catch((error) => {
-    console.log('Database connection failed !');
-    console.error(error);
-    process.exit(1);
-  });
+require('./config/database').connect();
 
 // SESSION STORE
 const store = new MongoDBStore({
@@ -34,6 +21,7 @@ const store = new MongoDBStore({
 app.set('view engine', 'ejs');
 
 // MIDDLEWARES
+app.use(express.static('public'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -53,15 +41,8 @@ app.use(
 );
 
 // ROUTES
-app.get('/login', pageController.getLoginPage);
-app.get('/signup', pageController.getSignupPage);
-app.get('/', auth, pageController.getIndexPage);
-app.get('/welcome', pageController.redirectToIndexPage);
-
-app.post('/register', userController.registerUser);
-app.post('/welcome', userController.checkUser);
-app.get('/list', auth, userController.listUsers);
-app.get('/logout', userController.logoutUser);
+app.use('/', pageRoute);
+app.use('/users', userRoute);
 
 // START THE SERVER
 const { API_PORT } = process.env;
